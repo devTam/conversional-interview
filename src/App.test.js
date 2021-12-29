d6cdddf040b983130694d2ce6f343c22d35ac9be
devTam <tammy.batubo@gmail.com>
2021-12-29 02:44:50 +0100

    add tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,15 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import ReactDOM from 'react-dom';
+
+test('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+});
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/I open a link but I should also open the modal./i);
+  console.log(screen)
   expect(linkElement).toBeInTheDocument();
 });
